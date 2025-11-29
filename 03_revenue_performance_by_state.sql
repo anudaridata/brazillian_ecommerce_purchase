a827c5f80f73db10f3cedcd2c50570b69a80d1f8
Anudari Chingunbat <164714826+anudaridata@users.noreply.github.com>
2025-11-29 16:44:55 +0900

    Update 03_revenue_performance_by_state.sql

--- 03_revenue_performance_by_state.sql
+++ 03_revenue_performance_by_state.sql
@@ -17,6 +17,45 @@ GROUP BY c.customer_state
 ORDER BY total_revenue DESC
 LIMIT 5;
 
+-- sub calculation on how to find how much percentage of total revenue the TOP 5 states account for 
+WITH state_revenue AS 
+(SELECT 
+        c.customer_state, 
+        CASE c.customer_state 
+            WHEN 'SP' THEN 'Sao Paulo'
+            WHEN 'RJ' THEN 'Rio de Janeiro'
+            WHEN 'MG' THEN 'Minas Gerais'
+            WHEN 'RS' THEN 'Rio Grande do Sul'
+            WHEN 'PR' THEN 'Parana'
+        END as state_name,
+        ROUND(SUM(p.payment_value), 2) as state_total_revenue
+    FROM payments p
+    JOIN orders o ON o.order_id = p.order_id
+    JOIN customers c ON c.customer_id = o.customer_id
+    WHERE o.order_status = 'delivered'
+    GROUP BY c.customer_state
+),
+
+top_5_states AS 
+(
+    SELECT ROUND(SUM(state_total_revenue),2) as top5_revenue
+    FROM (
+        SELECT state_total_revenue
+        FROM state_revenue
+        ORDER BY state_total_revenue DESC
+        LIMIT 5
+    )
+),
+
+total_revenue AS 
+(SELECT ROUND(SUM(state_total_revenue), 2) as sum_total_revenue
+    FROM state_revenue)
+
+SELECT CAST(top5_revenue as INTEGER) as top5_revenue, 
+CAST(sum_total_revenue as INTEGER) as sum_total_revenue,
+ROUND((top5_revenue * 100.0/ sum_total_revenue), 1) as percentage_of_total
+FROM top_5_states, total_revenue
+
 -- most popular product categories in each region
 SELECT ca.product_category_name_english, ROUND(SUM(ps.payment_value),2) as total_payment, 
 c.customer_state,
@@ -60,6 +99,9 @@ AND p.product_category_name IS NOT NULL
 GROUP BY p.product_category_name, c.customer_state
 ORDER BY c.customer_state, total_payment DESC
 LIMIT 10;
+
+
+
 
 
 
